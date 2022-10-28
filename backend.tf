64309a3bef51c19e06b6e1f96c8a1efcc014a31f
Suyog-Gowda <106002049+Suyog-Gowda@users.noreply.github.com>
2022-10-28 10:38:53 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,5 +1,5 @@
 terraform {
   backend "local" {
-     path = "C:\Users\SUYOG\OneDrive\Desktop\Terraform\Ec2\terraform.tfstate.d\dev
+     path = "C:/Users/SUYOG/OneDrive/Desktop/Terraform/Ec2/terraform.tfstate.d/dev
   }
 }
